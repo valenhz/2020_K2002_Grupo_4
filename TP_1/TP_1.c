9423657790c21401a423fc296a78c339b85a148c
ivanscardaoni1 <ivan.scardaoni1@gmail.com>
2020-04-21 11:14:26 -0300

    pruebo con unos archivos cualquiera para ver si compila y toma los archivos

--- TP_1/TP_1.c
+++ TP_1/TP_1.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
 
+//hago una prueba de archivos para despues usar en el programa
+
+
 int main(){
+    
+    FILE * archivo = fopen("hola.txt", "rb");
+    FILE * archivo2 = fopen("chau.txt","wb");
+    
+    if (archivo == NULL){
+        perror("Error en la apertura del archivo");
+        return 1;
+    }
+    char caracter;
+    while(feof(archivo)==0){
+        caracter = fgetc(archivo);
+        printf("%c", caracter);
+    }
+    
+    fprintf(archivo2, "%s", "Escribo algo");
+   
+    fclose(archivo);
+    fclose(archivo2);
+
+
     return 0;
 }
