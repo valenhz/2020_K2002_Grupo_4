101a7fd75cfd84efa1db2e287b15ae635d0a39e5
ivanscardaoni1 <ivan.scardaoni1@gmail.com>
2020-04-24 17:30:54 -0300

    Creamos e implementamos la funcion que aplica el automata

--- TP_1/TP_1.c
+++ TP_1/TP_1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Prototipo de funcion que aplica el automata.
+void aplicoAutomata (int [][7], char , int, int, int);
+
+
 int main(int argc, const char * argv[]) {
+
+//Declaramos e inicializamos el automata con 7 estados (hay un estado de palabra invalida).
+int automata[7][7] = {{2, 1, 1, 6, 6, 0, 6},{1, 1, 1, 6, 6, 0, 6},{5, 5, 6, 3, 6, 0, 6},{4, 4, 4, 6, 4, 0, 6},{4, 4, 4, 6, 4, 0, 6},{5, 5, 6, 6, 6, 0, 6},{6, 6, 6, 6, 6, 0, 6}};
 
 //abrimos archivo de entrada en modo lectura +.
 FILE * archivoEntrada = fopen("entrada.txt","r+");
@@ -16,13 +23,21 @@ if(archivoEntrada == NULL){
  
  //Variable que almacena cada caracter leido.
  char caracter;
- 
+
+  //variable que almacena el ultimo estado.
+ int ultimoEstado;
+ //variables que almacenan las posiciones del automata.
+int i = 0;
+int j = 0;
+
  //Ciclo en el que va a correr la implementacion del automata.
  while(feof(archivoEntrada) == 0) {
         caracter = fgetc(archivoEntrada);
 
-//Aca va todo el codigo que tiene que ver con la evaluacion de las palabras.
- 
+ //Implementacion de la funcion que aplica el automata.
+aplicoAutomata (automata, caracter, ultimoEstado, i, j);
+
+
  }
 
 
@@ -31,4 +46,17 @@ fclose(archivoEntrada);
 fclose(archivoSalida);
 
     return 0;
+}
+
+void aplicoAutomata (int M[][7], char a, int b, int i, int j){
+    if (a == '0'){j = 0;} else
+    if (a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7'){ j = 1;} else
+    if (a == '8' || a == '9'){j = 2;} else
+    if (a == 'x' || a == 'X'){ j = 3;} else
+    if (a == 'a' || a == 'b' || a == 'c' || a == 'd' || a == 'e' || a == 'f' || a == 'A' || a == 'B' || a == 'C' || a == 'D' || a == 'E' || a == 'F'){ j = 4;} else
+    if (a == ','){ j = 5;} else
+        { j = 6;}
+    b = i = M[i][j];
+    
+    
 }
