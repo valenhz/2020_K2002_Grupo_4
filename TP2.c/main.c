bfa48923e09da857cf3cabadaac0faaffb0581b9
valenhz <balvalentino@gmail.com>
2020-06-02 19:51:55 -0300

    cree la funcion ingresar para cargar la matriz

--- TP2.c/main.c
+++ TP2.c/main.c
@@ -2,24 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+estado ingresar (int, char*);
 
+typedef struct {             //estructura para generar la matriz 
+        int siguiente;
+        const char* cadPush [3];
+    }estado;
 
-int main (int argc, const char * argv[]) {
+int main (int argc, char * argv[]) {
 
     char expresion [30];        //expresion (cadena de caracteres)
     
     printf ("Ingrese la expresion: ");
     scanf ("%s", expresion);    //usuario ingresa la expresion
 
-    struct estado {             //estructura para generar la matriz 
-        int estadoSiguiente;
-        char cadPush [3];
-    };
-    
-    struct estado automata [3][2][5];
 
-    automata [0][0][0] = {3," "};
+    
+    estado automata [3][2][5];
+
+    automata[0][0][0].siguiente = ingresar (3, '$');
     
     
 return 0;
 }
+estado ingresar (int a, char* b) {
+estado x;
+  x.siguiente = a;
+  x.cadPush = b;
+  return x;
+  }
