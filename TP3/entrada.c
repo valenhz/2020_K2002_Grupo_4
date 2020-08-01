55710179f122e7e92053f042f66f48f25a4348be
celeslvp <celestefchallen@gmail.com>
2020-08-01 17:18:34 -0300

    definiciones y erx de comentarios de una linea

--- TP3/entrada.c
+++ TP3/entrada.c
@@ -1,11 +1,15 @@
-()
-[]
+while do awhile whilen
+
+int calabaza = 23;
+
+char casa = (5+5);
+
+if (a<10)
 {
+    a++;
 }
-5
-,
-;
-==
-=
-&&
-include
+
+//holisssssss qjwkeljq qlkwjeqklwe kjeqlwkejq qlkejqlke qlkwjqke hola letra int void 
+
+int void casa 55 //hola
+
