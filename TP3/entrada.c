3bbea98d27943d398d2f98c41edc8e696592eccf
celeslvp <celestefchallen@gmail.com>
2020-08-03 13:27:28 -0300

    Regla de caracteres no reconocidos

--- TP3/entrada.c
+++ TP3/entrada.c
@@ -4,14 +4,21 @@ int calabaza = 23;
 
 char casa = (5+5);
 
+'holissss'
+
 if (a<10)
 {
     a++;
 }
+
+..
+.
 
 {{{}}}
 []],,,
 //holisssssss qjwkeljq qlkwjeqklwe kjeqlwkejq qlkejqlke qlkwjqke hola letra int void 
 
 int void casa 55 //hola
+
+.......
 
